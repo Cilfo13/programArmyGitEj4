a9c4e5992eea9df5c920ad8d1007e2d73c8656ef
xMael-dev <martinfacundo9562@gmail.com>
2021-09-21 18:53:39 -0300

    Version2 Conejo

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 
 public class main {
     public static void main(String[] args) {
+        //Pongan las clases acá :)
         Conejo conejo = new Conejo("Copito","Blanco","Ni idea es un conejo blanco", 48.6, 10, true);
         System.out.println(conejo.toString());
     }    
