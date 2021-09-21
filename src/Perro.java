53184e2abd6625c68be8627725549ca9a306a409
PerezSebastian <psebastian10101010@gmail.com>
2021-09-21 20:49:45 -0300

    commit perro parte 2000

---
+++ src/Perro.java
@@ -0,0 +1,14 @@
+
+public class Perro {
+    
+    String nombre,raza,color;
+    int edad;
+
+    public Perro(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void ladrar() {
+        System.out.println("GUAU GUAU GUAU GUAU GUAU");
+    }
+}
